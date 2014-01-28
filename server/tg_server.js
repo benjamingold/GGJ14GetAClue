bc0149e62b53dfe59e85c96e88506a86dfc3268d
mildmojo <theaterfire@yahoo.com>
2014-01-28 00:43:18 -0500

    Fixes for Heroku deployment.

--- server/tg_server.js
+++ server/tg_server.js
@@ -76,4 +76,7 @@ function switchRooms(socket, roomName, oldRoom) {
   socket.emit('id', roomName);
 }
 
-server.listen(3000);
+var port = process.env.PORT || 3000;
+server.listen(port);
+
+console.log('Listening on port ' + port);
